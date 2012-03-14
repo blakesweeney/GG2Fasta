5e6483b4c38b5876b55d7d7b2881b664ec6083e6
Blake Sweeney <blakes.85@gmail.com>
2012-03-14 15:41:03 -0400

    A simple script to turn green genes files to fasta

---
+++ gg2fasta.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import re
+import argparse
+
+import simplejson as json
+
+
+class SplitAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        fields = values.split(',')
+        curr = getattr(namespace, self.dest) or []
+        curr.extend(fields)
+        setattr(namespace, self.dest, curr)
+
+parser = argparse.ArgumentParser(description='Convert GG format to fasta')
+parser.add_argument('gg_file', metavar='GG', type=file,
+                    help='Green Genes file to parse')
+parser.add_argument('--fields', action=SplitAction,
+                    help='Feilds to output in the fasta file')
+
+args = parser.parse_args()
+fields = args.fields
+entry_pattern = re.compile('([\w_]+)=(.*)$')
+
+record = {}
+for line in args.gg_file:
+    line.rstrip()
+    match = entry_pattern.match(line)
+    if match:
+      record[match.group(1)] = match.group(2)
+    if line == 'END\n':
+      sequence = record['aligned_seq']
+      del record['aligned_seq']
+      print('> %s' % json.dumps(record))
+      print(sequence)
+      record = {}
